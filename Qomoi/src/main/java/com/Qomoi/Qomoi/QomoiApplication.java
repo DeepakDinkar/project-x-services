068b7a970ec98b6f24d8b52f752a173b5a98eb6c
Harish <harish@telebright.com>
2024-01-08 18:14:01 +0530

    Application Added

--- Qomoi/src/main/java/com/Qomoi/Qomoi/QomoiApplication.java
+++ Qomoi/src/main/java/com/Qomoi/Qomoi/QomoiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QomoiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QomoiApplication.class, args);
 	}
