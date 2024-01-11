82c38b408fe18c9677b2776d9eb65b1d70668a05
Harish <harish@telebright.com>
2024-01-11 10:49:31 +0530

    Signup Response and check email already exists

--- Qomoi/src/main/java/com/Qomoi/Qomoi/Controller/AuthenticationController.java
+++ Qomoi/src/main/java/com/Qomoi/Qomoi/Controller/AuthenticationController.java
@@ -2,12 +2,16 @@ package com.Qomoi.Qomoi.Controller;
 
 
 import com.Qomoi.Qomoi.Entity.UserEntity;
+import com.Qomoi.Qomoi.Repository.UserRepository;
 import com.Qomoi.Qomoi.Request.SigninRequest;
 import com.Qomoi.Qomoi.Request.SignupRequest;
 import com.Qomoi.Qomoi.Response.JWTAuthenticationResponse;
+import com.Qomoi.Qomoi.Response.SignupResponse;
 import com.Qomoi.Qomoi.Service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,6 +23,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -28,15 +34,34 @@ public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
 
+    private final UserRepository userRepository;
 
     @PostMapping("/user")
-    public ResponseEntity<UserEntity> signupUser(@RequestBody SignupRequest signupRequest){
-        return ResponseEntity.ok(authenticationService.signupUser(signupRequest));
+    public ResponseEntity<SignupResponse> signupUser(@RequestBody SignupRequest signupRequest) {
+        Optional<UserEntity> existingUser = userRepository.findByEmail(signupRequest.getEmail());
+        if (existingUser.isPresent()) {
+            String message = "User with email " + signupRequest.getEmail() + " already exists";
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(new SignupResponse(message, null));
+        } else {
+            UserEntity newUser = authenticationService.signupUser(signupRequest);
+            String message = "Successfully registered";
+            SignupResponse response = new SignupResponse(message, newUser.getEmail());
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }
     }
 
     @PostMapping("/admin")
-    public ResponseEntity<UserEntity> signupAdmin(@RequestBody SignupRequest signupRequest){
-        return ResponseEntity.ok(authenticationService.signupAdmin(signupRequest));
+    public ResponseEntity<SignupResponse> signupAdmin(@RequestBody SignupRequest signupRequest){
+        Optional<UserEntity> existingUser = userRepository.findByEmail(signupRequest.getEmail());
+        if (existingUser.isPresent()) {
+            String message = "Admin with email " + signupRequest.getEmail() + " already exists";
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(new SignupResponse(message, null));
+        } else {
+            UserEntity newUser = authenticationService.signupAdmin(signupRequest);
+            String message = "Successfully registered";
+            SignupResponse response = new SignupResponse(message, newUser.getEmail());
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }
     }
 
     @PostMapping("/signIn")
