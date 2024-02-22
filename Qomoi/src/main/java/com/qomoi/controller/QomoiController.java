503cff6a731cd2ec7b82d9a03637f182a5690b8b
Harish <harish@telebright.com>
2024-02-22 14:15:10 +0530

    commit-3 22/02/24

---
+++ Qomoi/src/main/java/com/qomoi/controller/QomoiController.java
@@ -0,0 +1,17 @@
+package com.qomoi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/qomoi")
+public class QomoiController {
+
+    @PostMapping("/callback")
+    public ResponseEntity<?> callBack(){
+        return null;
+    }
+
+}
